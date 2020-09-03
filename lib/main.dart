3e43f077d4697aa9a4e21085cf916ac583a009c8
coderkeke <18290060679@163.com>
2020-09-03 14:05:45 +0800

    初始化项目

--- lib/main.dart
+++ lib/main.dart
@@ -4,39 +4,4 @@ import 'package:flutter/material.dart';
 //   runApp(MyApp());
 // }
 
-main(List<String> args) => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Hello World",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("我是标题"),
-        ),
-        body: ContentWidget(),
-      ),
-    );
-  }
-}
-
-class ContentWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: TextWidget(),
-    );
-  }
-}
-
-class TextWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "Hello World2",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(fontSize: 30, color: Colors.orange),
-    );
-  }
-}
+main(List<String> args) => runApp();

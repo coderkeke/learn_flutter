96ce3709396e34d651a6168c57f708ec837c9aec
coderkeke <18290060679@163.com>
2020-09-08 13:32:08 +0800

    基础案例

--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,100 @@ import 'package:flutter/material.dart';
 //   runApp(MyApp());
 // }
 
-main(List<String> args) => runApp();
+main(List<String> args) => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Hello World",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("我是标题"),
+        ),
+        body: ContentWidget(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print(1231);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ContentWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ContentWidgetState();
+  }
+}
+
+class ContentWidgetState extends State<ContentWidget> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                onPressed: () {
+                  print("按钮点击");
+                  setState(() {
+                    counter++;
+                  });
+                },
+                child: Text("计数+1"),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  print("按钮点击");
+                  setState(() {
+                    counter--;
+                  });
+                },
+                child: Text("计数-1"),
+              ),
+            ],
+          ),
+          Text(
+            "当前计数: $counter",
+            style: TextStyle(fontSize: 25),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//无法实现
+class ContentWidget1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RaisedButton(
+            onPressed: () {
+              print("按钮点击");
+            },
+            child: Text("计数+1"),
+          ),
+          Text(
+            "当前计数: 0",
+            style: TextStyle(fontSize: 25),
+          ),
+        ],
+      ),
+    );
+  }
+}
